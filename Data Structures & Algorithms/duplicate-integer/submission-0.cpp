58b9a976b1c26a688c7dcc8a99f9402116ba3f79
Aryan-Mishra <aryanmishra43329@gmail.com>
2026-08-07 01:39:59 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int num:nums){
+            mp[num]++;
+        }
+        for(const auto& pair:mp){
+            if(pair.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
