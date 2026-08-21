f5d4316333b8dfcf1c06b4592362a0e2ddc300fc
Aryan-Mishra <aryanmishra43329@gmail.com>
2026-08-22 00:52:58 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                char top=st.top();
+                if(top=='(' && s[i]!=')'){
+                    return false;
+                }else if(top=='{' && s[i]!='}'){
+                    return false;
+                }else if(top=='[' && s[i]!=']'){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
